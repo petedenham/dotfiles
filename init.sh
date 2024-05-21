187407ce758ec163c26fa0559558912628badebd
Peter Denham <hello@peterdenham.co.uk>
2024-05-21 21:27:10 +0100

    add homebrew installation to the init script

--- init.sh
+++ init.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env bash
 
+# Ask for the admin password upfront
+sudo -v
+
+# Install Homebrew
+yes '' | /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# Copy dotfiles and config files to home folder
 stow --adopt .
