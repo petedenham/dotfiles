3641cc68917f98cc0511578e18191205d1d54525
Peter Denham <hello@peterdenham.co.uk>
2025-01-02 09:49:26 +0000

    move lazy load setup to the init file

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,11 +1,18 @@
-vim.cmd([[
-  syntax off
-  filetype off
-  filetype plugin indent off
-]])
-
 require("core")
 require("setup")
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
+end
+vim.opt.rtp:prepend(lazypath)
 
 vim.filetype.add({
 	filename = {
@@ -13,10 +20,17 @@ vim.filetype.add({
 	},
 })
 
-vim.defer_fn(function()
-	vim.cmd([[
-      syntax on
-      filetype on
-      filetype plugin indent on
-  ]])
-end, 0)
+require("lazy").setup({
+	{ import = "plugins" },
+}, {
+	install = {
+		colorscheme = { "dracula" },
+	},
+	checker = {
+		enabled = true,
+		notify = false,
+	},
+	change_detection = {
+		notify = false,
+	},
+})
